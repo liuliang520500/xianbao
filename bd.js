cdb3d659023959bcda7d95998cef369406a64a52
liuliangzheng <liuliangzheng520@163.com>
2022-04-25 11:43:25 +0800

    firts

---
+++ bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?025f32dd85ca7f0486c1c6bbb1b16360";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
